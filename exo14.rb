88f323454339b30af4290066179daf6c9f32b570
Jo9085 <josephine.pean@gmail.com>
2023-01-16 14:37:04 +0100

    even numbers

---
+++ exo14.rb
@@ -0,0 +1,22 @@
+puts "Quel est ton prénom ?"
+print ">"
+user_firstname=gets.chomp
+
+puts "Et ton nom ?"
+print ">"
+user_lastname=gets.chomp
+
+mails = Array.new
+
+50.times do |i|
+    if i < 9
+        email_address="#{user_firstname}.#{user_lastname}0#{i + 1}@gmail.com"
+    else
+        email_address="#{user_firstname}.#{user_lastname}#{i + 1}@gmail.com"
+    end
+    mails [i]= email_address
+
+    if i.even? == false
+        puts "#{email_address}"
+    end
+end
